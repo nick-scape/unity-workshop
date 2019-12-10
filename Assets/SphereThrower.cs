abbabe8a8c5d64c20d5f3367abe750fb2d32b6dd
nick-scape <50655518+nick-scape@users.noreply.github.com>
2019-12-10 16:18:23 +0000

    instantiate prefab

---
+++ Assets/SphereThrower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereThrower : MonoBehaviour
+{
+	public GameObject sphere;
+
+	public float startSpeed = 100.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (Input.GetKeyDown("space"))
+        {
+            GameObject instance = Instantiate(sphere);
+
+            Rigidbody rb = instance.GetComponent<Rigidbody>() as Rigidbody;
+
+            Vector3 startPos = this.gameObject.transform.position + 
+            (this.gameObject.transform.forward * -2.0f);
+            startPos.y += 100.0f;
+
+            instance.transform.position = startPos;
+
+            rb.velocity = this.gameObject.transform.forward * -startSpeed;
+        }  
+    }
+}
